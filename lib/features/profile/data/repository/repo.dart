e6fcd27c04b8fc6df138831a9249d0a921b5528f
Ahmed <benzemaa46@gmail.com>
2026-03-10 09:11:54 +0200

    implementation of profile repo with get user data

--- lib/features/profile/data/repository/repo.dart
+++ lib/features/profile/data/repository/repo.dart
@@ -1,1 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:textract/core/models/user_model.dart';
+import 'package:textract/core/utils/typedef.dart';
+import 'package:textract/features/profile/data/data_source/data_source.dart';
 
+abstract class ProfileRepository {
+  ServerResponse<UserModel> getUserData();
+}
+
+class ProfileRepositoryImpl implements ProfileRepository {
+  final ProfileDataSource _dataSource;
+  ProfileRepositoryImpl(this._dataSource);
+  @override
+  ServerResponse<UserModel> getUserData() async {
+    try {
+      final response = await _dataSource.getUserData();
+      return Right(response);
+    } catch (e) {
+      return Left(e.toString());
+    }
+  }
+}
